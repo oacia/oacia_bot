3aa527de42bed154e13781ddc7ff0b4c307273a6
oacia <93508519+oacia@users.noreply.github.com>
2023-12-30 17:34:27 +0800

    vercel test

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ bot = telegram.Bot(token=bot_token)
 
 
 @app.route('/callback', methods=['POST'])
-async def webhook_handler():
+def webhook_handler():
     """Set route /callback with POST method will trigger this method."""
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
@@ -44,13 +44,13 @@ async def webhook_handler():
     return 'ok'
 
 @app.route('/')
-async def home():
+def home():
     return 'hello world'
 
 
 
 # 抖音视频无水印
-async def videos(surl, update):
+def videos(surl, update):
     id = re.search(r'video/(\d+)', surl).group(1)
     # print(id)
     # 获取json数据
@@ -60,15 +60,15 @@ async def videos(surl, update):
     req = v_rs['item_list'][0]['video']['play_addr']['uri']
     v_url = "https://www.douyin.com/aweme/v1/play/?video_id={}".format(req)
     #print(f"{user}: [send] {req}.mp4")
-    await update.message.reply_text("1 vedio downloading...")
-    await update.message.reply_video(v_url)
+    update.message.reply_text("1 vedio downloading...")
+    update.message.reply_video(v_url)
     #await event.reply("1 vedio downloading...")
     # 将视频下载的压力转移给gelegram服务器:)
     #await client.send_file(user, v_url, video_note=True)
 
 
 # 抖音图片无水印
-async def pics(surl, update):
+def pics(surl, update):
     pid = re.search(r'note/(\d+)', surl).group(1)
     # 获取json数据
     p_id = "https://m.douyin.com/web/api/v2/aweme/iteminfo/?reflow_source=reflow_page&item_ids={}&a_bogus=".format(pid)
@@ -77,7 +77,7 @@ async def pics(surl, update):
     # print(p_rs)
     # 拿到images下的原图片
     images = p_rs['item_list'][0]['images']
-    await update.message.reply_text(f"{len(images)} picture downloading...")
+    update.message.reply_text(f"{len(images)} picture downloading...")
     #await event.reply(f"{len(images)} picture downloading...")
     for i, im in enumerate(images):
         p_req = requests.get(url=im['url_list'][0])
@@ -87,12 +87,12 @@ async def pics(surl, update):
             photo.write(data)
         photo.seek(0, 0)
         #print(f"{user}: [send] {im['url_list'][0]}")
-        await update.message.reply_photo(photo)
+        update.message.reply_photo(photo)
         #await client.send_file(user, photo)
 
 
 #@client.on(events.NewMessage(pattern='/start'))
-async def start(bot, update):
+def start(bot, update):
     user = update.effective_user
     response = f"hello! {user.username}"
     response += '''
@@ -101,11 +101,11 @@ async def start(bot, update):
     1 - download douyin vedio or pictures by sending a shared link
     
     source code:https://github.com/oacia/oacia_bot'''
-    await update.message.reply_text(response)
+    update.message.reply_text(response)
 
 
 #@client.on(events.NewMessage(pattern=r'.*v\.douyin\.com.*'))
-async def douyin(bot, update):
+def douyin(bot, update):
     user = update.effective_user
     print(f"{user.username}: [receive] msg{update.message.text}")
     share = re.search(r'/v.douyin.com/(.*?)/', update.message.text).group(1)
@@ -115,10 +115,10 @@ async def douyin(bot, update):
     # 获取重定向后的视频id
     surl = s_html.url
     if re.search(r'/video', surl) != None:
-        await videos(surl, update)
+        videos(surl, update)
         # 判断链接类型为图集分享类型
     elif re.search(r'/note', surl) != None:
-        await pics(surl, update)
+        pics(surl, update)
 
 dispatcher = Dispatcher(bot, None)
 dispatcher.add_handler(CommandHandler("start", start))

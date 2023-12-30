3943c863cd46bf8417f8967b46072fc5e232cfb9
oacia <93508519+oacia@users.noreply.github.com>
2023-12-30 16:07:41 +0800

    vercel test

--- main.py
+++ main.py
@@ -1,60 +1,42 @@
-from telethon import TelegramClient, events
-from telethon.sessions import StringSession
-import json
+
 import re
 import requests
 from io import BytesIO
 import os
 from flask import Flask, request
-
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters,StringRegexHandler
+from telegram import ForceReply, Update
 # 请求头
 header = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Safari/537.36"}
-
-# # just for my local test...
-# config_path = ""
-# if os.path.exists('./secret'):
-#     config_path = "secret/"
-#
-# with open(f"{config_path}config.json", "r") as file:
-#     credentials = json.loads(file.read())
-#
-# api_id = credentials["API_ID"]
-# api_hash = credentials["API_HASH"]
-# bot_token = credentials["BOT_TOKEN"]
-# session = credentials["SESSION"]
-# proxy = credentials["PROXY"]  # set proxy,eg.["HTTP", "127.0.0.1", 7890]
 
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
 session = os.getenv("SESSION")
 
-#app = Flask(__name__)
+app = Flask(__name__)
+bot = Application.builder().token(bot_token).build()
 
-client = TelegramClient(session=StringSession(session), api_id=api_id, api_hash=api_hash).start(bot_token=bot_token)
-client.connect()
+# client = TelegramClient(session=StringSession(session), api_id=api_id, api_hash=api_hash).start(bot_token=bot_token)
+# client.connect()
 
-# @app.route('/callback', methods=['POST'])
-# def webhook_handler():
-#     """Set route /hook with POST method will trigger this method."""
-#     # app.logger.info("receive message")
-#     # client.send_message('oacia', "6666666")
-#     if request.method == "POST":
-#         app.logger.info(request.get_json(force=True))
-#         #client.
-#         # client.send_message('oacia', "12345678")
-#         # client.send_message('oacia', request.get_json(force=True))
-#     return 'ok'
+@app.route('/callback', methods=['POST'])
+def webhook_handler():
+    """Set route /callback with POST method will trigger this method."""
+    if request.method == "POST":
+        update = Update.de_json(request.get_json(force=True), bot)
+        bot.process_update(update)
+    return 'ok'
 
 
-# @app.route('/')
-# def home():
-#     return 'hello world'
+@app.route('/')
+def home():
+    return 'hello world'
 
 
 # 抖音视频无水印
-async def videos(surl, user, event):
+async def videos(surl, update: Update):
     id = re.search(r'video/(\d+)', surl).group(1)
     # print(id)
     # 获取json数据
@@ -63,14 +45,16 @@ async def videos(surl, user, event):
     # 获取uri参数
     req = v_rs['item_list'][0]['video']['play_addr']['uri']
     v_url = "https://www.douyin.com/aweme/v1/play/?video_id={}".format(req)
-    print(f"{user}: [send] {req}.mp4")
-    await event.reply("1 vedio downloading...")
+    #print(f"{user}: [send] {req}.mp4")
+    await update.message.reply_text("1 vedio downloading...")
+    await update.message.reply_video(v_url)
+    #await event.reply("1 vedio downloading...")
     # 将视频下载的压力转移给gelegram服务器:)
-    await client.send_file(user, v_url, video_note=True)
+    #await client.send_file(user, v_url, video_note=True)
 
 
 # 抖音图片无水印
-async def pics(surl, user, event):
+async def pics(surl, update: Update):
     pid = re.search(r'note/(\d+)', surl).group(1)
     # 获取json数据
     p_id = "https://m.douyin.com/web/api/v2/aweme/iteminfo/?reflow_source=reflow_page&item_ids={}&a_bogus=".format(pid)
@@ -79,7 +63,8 @@ async def pics(surl, user, event):
     # print(p_rs)
     # 拿到images下的原图片
     images = p_rs['item_list'][0]['images']
-    await event.reply(f"{len(images)} picture downloading...")
+    await update.message.reply_text(f"{len(images)} picture downloading...")
+    #await event.reply(f"{len(images)} picture downloading...")
     for i, im in enumerate(images):
         p_req = requests.get(url=im['url_list'][0])
         photo = BytesIO()
@@ -87,41 +72,44 @@ async def pics(surl, user, event):
         for data in p_req.iter_content(chunk_size=1024):
             photo.write(data)
         photo.seek(0, 0)
-        print(f"{user}: [send] {im['url_list'][0]}")
-        await client.send_file(user, photo)
+        #print(f"{user}: [send] {im['url_list'][0]}")
+        await update.message.reply_photo(photo)
+        #await client.send_file(user, photo)
 
 
-@client.on(events.NewMessage(pattern='/start'))
-async def start(event):
-    sender = await client.get_entity(event.peer_id.user_id)
-    response = f"hello! {sender.username}"
+#@client.on(events.NewMessage(pattern='/start'))
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    response = f"hello! {user.username}"
     response += '''
     this is a bot create by oacia
     you can:
     1 - download douyin vedio or pictures by sending a shared link
     
     source code:https://github.com/oacia/oacia_bot'''
-    await event.reply(response)
+    await update.message.reply_text(response)
 
 
-@client.on(events.NewMessage(pattern=r'.*v\.douyin\.com.*'))
-async def readMessages(event):
-    user = await client.get_entity(event.peer_id.user_id)
-    print(f"{user.username}: [receive] msg{event.text}")
-    share = re.search(r'/v.douyin.com/(.*?)/', event.text).group(1)
+#@client.on(events.NewMessage(pattern=r'.*v\.douyin\.com.*'))
+async def douyin(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    print(f"{user.username}: [receive] msg{update.message.text}")
+    share = re.search(r'/v.douyin.com/(.*?)/', update.message.text).group(1)
     # 请求链接
     share_url = "https://v.douyin.com/{}/".format(share)
     s_html = requests.get(url=share_url, headers=header)
     # 获取重定向后的视频id
     surl = s_html.url
     if re.search(r'/video', surl) != None:
-        await videos(surl, user.username, event)
+        await videos(surl, update)
         # 判断链接类型为图集分享类型
     elif re.search(r'/note', surl) != None:
-        await pics(surl, user.username, event)
+        await pics(surl, update)
 
 
 # Run the event loop to start receiving messages
-client.run_until_disconnected()
-# if __name__ == '__main__':
-#     app.run()
+# client.run_until_disconnected()
+if __name__ == '__main__':
+    app.run()
+    bot.add_handler(CommandHandler("start", start))
+    bot.add_handler(MessageHandler(filters.Regex(r'.*v\.douyin\.com.*'),douyin))
